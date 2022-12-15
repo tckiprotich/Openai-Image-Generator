418e4b46d66a45e3eb6286505282febee284ce5c
tckiprotich <tckiprotich@gmail.com>
2022-12-15 14:14:41 +0300

    qwerty

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const openai = require('../controllers/openai');
 
 // Path: routes\index.js
-router.get('/index', (req, res) => { // This is the route that will be called when the user goes to the home page
+router.get('/', (req, res) => { // This is the route that will be called when the user goes to the home page
     res.render('index'); // This is the view that will be rendered
 });
 
